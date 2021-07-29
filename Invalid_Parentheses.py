64d2deaa73bff0b9986123ff034985148ae44788
Yashassvi Suhane <87795720+yashuswe@users.noreply.github.com>
2021-07-29 11:38:31 +0530

    Add files via upload

---
+++ Invalid_Parentheses.py
@@ -0,0 +1,16 @@
+#Invalid Parentheses
+#An expression will be given which can contain open and close parentheses ,
+#No other operator will be there in string. Find the paranthesis patterns is valid or not
+n=input()
+c=0
+for i in n:
+    if i == '(':
+        c += 1
+    elif i == ')':
+        c -= 1
+        if c < 0:
+            break
+if c == 0:
+    print("'True'")
+else:
+    print("'False'")     
